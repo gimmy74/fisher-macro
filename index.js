e4439870bf9e2df49f95726f19489807ec12ee92
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2022-07-01 04:39:05 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-const Client = require('./src/Fisher');
+const Fisher = require('./src/Fisher.js');
+const client = new Fisher();
 
-module.exports = Client;
+client.fish("4")
+
+client.on('Fished', data => {
+  console.log(`
+Fishing on an interval of ${data.interval}. 
+In channel ${data.id}.
+`)
+});
+
+
+
+
+client.sell("all", 1800)
+
+client.on('Sold', data => {
+  console.log(`
+Sold ${data.amount} Fish. 
+In channel ${data.id}.
+Interval of ${data.interval}.
+`)
+});
