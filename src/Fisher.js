59cd73c0051a92d2f392d3ca3654b6eee06d829f
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2022-07-01 04:41:57 -0400

    Create Fisher.js

---
+++ src/Fisher.js
@@ -0,0 +1,96 @@
+const Request = require('request');
+const EventEmitter = require('events');
+
+
+const link = require('../assets/links.js');
+
+const Utils = require('../assets/dontTouch.js');
+
+
+const prefix = "f";
+const token = process.env.token;
+const id = process.env.id;
+class Fisher extends EventEmitter {
+  constructor() {
+    super();
+  };
+
+  // Run Fish Command
+
+  async fish(seconds) {
+
+    const data = {
+      interval: seconds + "s",
+      id: id,
+    }
+
+    if (!seconds || seconds.toString().includes('-')) {
+      throw new Error("Must Provide A Valid Number In Seconds To Run Script At")
+    }
+
+    if (seconds < 2.6 || seconds > 10) {
+      throw new Error("Seconds Must Be Over The Bots Cooldown" + " " + `\(2.6s)` + " " + "Or Over The Limit" + " " + `\(10s).`)
+    }
+
+    setInterval(function() {
+      Request.post(link.API + id + "/messages", {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": token
+        },
+        body: JSON.stringify({ "content": prefix + "f" })
+      }, function(err, res, body) {
+
+      });
+
+    }, seconds * 1000)
+    this.emit('Fished', data)
+  };
+  //                   Sell Fish
+
+
+  async sell(amount, seconds) {
+
+    const data = {
+      interval: seconds + "s",
+      amount: amount,
+      id: id,
+    }
+    if (!amount || amount.toString().includes('-') || amount !== "all") {
+      throw new Error("Must Provide A Valid Amount Of Fish To Sell")
+    }
+
+
+
+    if (!seconds || seconds.toString().includes('-')) {
+      throw new Error("Must Provide A Valid Number In Seconds To Run Script At")
+    }
+
+    if (seconds < 10 || seconds > 6000) {
+      throw new Error("Seconds Must Be Over The Bots Cooldown" + " " + `\(2.6s)` + " " + "Or Over The Limit" + " " + `\(10s).`)
+    }
+
+
+
+    setInterval(function() {
+      Request.post(link.API + id + "/messages", {
+
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": process.env.token
+        },
+        body: JSON.stringify({ "content": prefix + "s" + " " + amount })
+
+      }, function(err, res, body) {
+
+      });
+
+    }, seconds * 1000)
+    
+    this.emit('Sold', data)
+    
+  }
+
+
+};
+module.exports = Fisher
