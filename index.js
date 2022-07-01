504e29dbb43d5ef5ecda1223aca0c7d35c1bd62a
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2022-07-01 15:56:15 -0400

    .

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const client = new Fisher();
 client.fish("4")
 
 client.on('Fished', data => {
+  
   console.log(`
 Fishing on an interval of ${data.interval}. 
 In channel ${data.id}.
 `)
+  
 });
 
 
@@ -16,9 +18,11 @@ In channel ${data.id}.
 client.sell("all", 1800)
 
 client.on('Sold', data => {
+  
   console.log(`
 Sold ${data.amount} Fish. 
 In channel ${data.id}.
 Interval of ${data.interval}.
 `)
+  
 });
