b5e7983dfb24ab238f1ea59a77d167dbb914fe92
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2022-07-02 04:56:46 -0400

    Rename folder

--- tests/fish.js
+++ tests/fish.js
@@ -1,6 +1,6 @@
 const Fisher = require('./src/Fisher.js');
 const client = new Fisher();
-
+https://github.com/gimmy74/fisher-macro/tree/main/commands
 client.fish(// number from 3s - 10s to run command at)
 
 client.on('Fished', fishing => {
