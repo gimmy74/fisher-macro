a8843a1988c87eafc824f0e433f0195fb937115b
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2022-07-01 21:39:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-const Fisher = require('./src/Fisher.js');
-const client = new Fisher();
+const Client = require('./src/Fisher');
 
-client.fish("4")
-
-client.on('Fished', data => {
-  
-  console.log(`
-Fishing on an interval of ${data.interval}. 
-In channel ${data.id}.
-`)
-  
-});
-
-
-
-
-client.sell("all", 1800)
-
-client.on('Sold', data => {
-  
-  console.log(`
-Sold ${data.amount} Fish. 
-In channel ${data.id}.
-Interval of ${data.interval}.
-`)
-  
-});
+module.exports = Client;
